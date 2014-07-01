1a5a4184ce0ef910ee3204b4f70a5b664976746d
Mihai Frisan <mihai@frisan.ro>
2014-07-01 12:38:22 +0300

    initial commit

---
+++ source/javascripts/script.js
@@ -0,0 +1,14 @@
+//= require js/jquery.flexslider-min
+
+$(document).ready(function() {
+  if(typeof console == 'undefined') {
+    this.console = { log: function() { } };
+  }
+});
+
+$(window).load(function() {
+    $('.flexslider').flexslider({
+        animation: "slide",
+        controlNav: "thumbnails"
+    });
+});
